118d39d7588602bea7e7c05f07a8126469d07b6e
jesonrijal <jesonrijal@7f01abe5-c8d1-403e-a4c0-fac70f8eeb35>
2019-02-05 23:26:14 +0000

    un update

--- Webproject_Rijal_0205/src/MyServletRijal0205.java
+++ Webproject_Rijal_0205/src/MyServletRijal0205.java
@@ -27,7 +27,7 @@ public class MyServletRijal0205 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.setContentType("text/html");
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
